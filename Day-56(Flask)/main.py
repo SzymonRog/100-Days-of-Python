3e124b8236a255710d6fc42361ce30fc8faeb8ed
Szymon <rogalaszymon78@gmail.com>
2025-10-16 19:09:22 +0200

    Compleat Day 56

--- Day-56(Flask)/main.py
+++ Day-56(Flask)/main.py
@@ -1,1 +1,12 @@
 # No time today but tomorrow I will learn how to use Flask
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
